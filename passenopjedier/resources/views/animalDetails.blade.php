936eb651a9fb109a6881a725b1d59d41fb0a2a54
LarsKoning <koninglars04@gmail.com>
2023-04-29 13:34:12 +0200

    styling animalDetails... Almost ig

--- passenopjedier/resources/views/animalDetails.blade.php
+++ passenopjedier/resources/views/animalDetails.blade.php
@@ -1,9 +1,13 @@
 @extends('dashboard')
 @section('content')
-<img src="/assets/Animals/{{$search->mediaAnimal->media}}" class="pfp" alt="L bozo">
-<h1>{{$animal->name}}, {{$animal->sort}}</h1>
-<h2>{{$animal->age}} Years Old</h2>
-<p>{{$animal->note}}</p>
-<p>From: {{substr($search->from,0,-3)}} <br> To: {{substr($search->to,0,-3)}} </p>
-<p>${{$search->payment}}</p>
+<div class="container">
+  <img src="/assets/Animals/{{$search->mediaAnimal->media}}" class="pfp" alt="L bozo">
+  <h1 class="tekstje">{{$animal->name}}, The {{$animal->sort}}</h1>
+
+  <h2 class="parags">Description Of {{$animal->name}}: </h2>
+  <p class="parags2"> {{$animal->name}} Is {{$animal->age}} Years Old. <br>{{$animal->note}}</p>
+
+  <p class="parag">{{$animal->name}} needs to be taken care of between: {{$search->from}} -> {{$search->to}}</p>
+  <p class="parag">The Price We Will Give You Is: €{{$search->payment}}</p>
+</div>
 @endsection
